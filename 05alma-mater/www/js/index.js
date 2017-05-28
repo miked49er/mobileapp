bca1666eca5e514a38d0b8b3f2289b58508dc6c6
Mike Deiters <miked49er@gmail.com>
2017-05-28 19:05:30 -0400

    Started to add functionality

--- 05alma-mater/www/js/index.js
+++ 05alma-mater/www/js/index.js
@@ -1,6 +1,64 @@
+var selectedButton = 'selected';
+
+function log(msg) {
+  console.log(msg);
+}
+
+function updateSelected(id) {
+  var buttons = document.getElementsByClassName(selectedButton);
+
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].classList.toggle(selectedButton, false);
+  }
+
+  var newButton = document.getElementById(id);
+  newButton.classList.toggle(selectedButton, true);
+}
+
+function instrumental() {
+  updateSelected('instrumental');
+}
+
+function lowVocal() {
+  updateSelected('low');
+}
+
+function highVocal() {
+  updateSelected('high');
+}
+
+function vocal() {
+  updateSelected('vocal');
+}
+
+function trackPlay() {
+  log('play');
+}
+
+function trackPause() {
+  log('pause');
+}
+
+function trackStop() {
+  log('stop');
+}
+
+function setupEventListeners() {
+  document.getElementById('instrumental').addEventListener('click', instrumental);
+  document.getElementById('low').addEventListener('click', lowVocal);
+  document.getElementById('high').addEventListener('click', highVocal);
+  document.getElementById('vocal').addEventListener('click', vocal);
+
+  document.getElementById('play').addEventListener('click', trackPlay);
+  document.getElementById('pause').addEventListener('click', trackPause);
+  document.getElementById('stop').addEventListener('click', trackStop);
+  instrumental();
+}
+
 var app = {
     // Application Constructor
     initialize: function() {
+      setupEventListeners();
     },
 
     // deviceready Event Handler
