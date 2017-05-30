1303b648e8b7f61f4664d75f4439eaba8c2aef55
Mike Deiters <miked49er@gmail.com>
2017-05-29 20:13:54 -0400

    Lyric display setup

--- 05alma-mater/www/js/index.js
+++ 05alma-mater/www/js/index.js
@@ -1,7 +1,107 @@
 var selectedButton = 'selected';
+var timecode = 0;
+var length = 0;
 
 function log(msg) {
   console.log(msg);
+}
+
+function getLyrics() {
+  var elem = document.getElementsByClassName(selectedButton)[0];
+  var lyrics;
+  switch (elem.id) {
+    case 'instrumental':
+      lyrics = [
+        {delta: 0, lyric: '[Introduction]'},
+        {delta: 8, lyric: 'We have gained wisdom and honor'},
+        {delta: 13, lyric: 'From our home of green and grey'},
+        {delta: 17, lyric: 'We will go forth and remember'},
+        {delta: 22, lyric: 'All we\'ve learned along the way'},
+        {delta: 25, lyric: 'And with knowledge and compassion'},
+        {delta: 29, lyric: 'We will build communities'},
+        {delta: 34, lyric: 'Leading by example'},
+        {delta: 39, lyric: 'And with dignity'},
+        {delta: 42, lyric: 'Georgia Gwinnett, we\'ll never forget'},
+        {delta: 51, lyric: 'How we have grown, and those that we\'ve met'},
+        {delta: 59, lyric: 'Georgia Gwinnett, with love and respect'},
+        {delta: 67, lyric: 'Our alma mater, Georgia Gwinnett'},
+        {delta: 75, lyric: 'Our alma mater, Georgia Gwinnett'}
+      ];
+      break;
+    case 'low':
+      lyrics = [
+        {delta: 0, lyric: '[Introduction]'},
+        {delta: 8, lyric: 'We have gained wisdom and honor'},
+        {delta: 13, lyric: 'From our home of green and grey'},
+        {delta: 17, lyric: 'We will go forth and remember'},
+        {delta: 22, lyric: 'All we\'ve learned along the way'},
+        {delta: 25, lyric: 'And with knowledge and compassion'},
+        {delta: 29, lyric: 'We will build communities'},
+        {delta: 34, lyric: 'Leading by example'},
+        {delta: 39, lyric: 'And with dignity'},
+        {delta: 42, lyric: 'Georgia Gwinnett, we\'ll never forget'},
+        {delta: 51, lyric: 'How we have grown, and those that we\'ve met'},
+        {delta: 59, lyric: 'Georgia Gwinnett, with love and respect'},
+        {delta: 67, lyric: 'Our alma mater, Georgia Gwinnett'},
+        {delta: 75, lyric: 'Our alma mater, Georgia Gwinnett'}
+      ];
+      break;
+    case 'high':
+      lyrics = [
+        {delta: 0, lyric: '[Introduction]'},
+        {delta: 11, lyric: 'We have gained wisdom and honor'},
+        {delta: 14, lyric: 'From our home of green and grey'},
+        {delta: 19, lyric: 'We will go forth and remember'},
+        {delta: 22, lyric: 'All we\'ve learned along the way'},
+        {delta: 27, lyric: 'And with knowledge and compassion'},
+        {delta: 31, lyric: 'We will build communities'},
+        {delta: 35, lyric: 'Leading by example'},
+        {delta: 40, lyric: 'And with dignity'},
+        {delta: 44, lyric: 'Georgia Gwinnett, we\'ll never forget'},
+        {delta: 51, lyric: 'How we have grown, and those that we\'ve met'},
+        {delta: 59, lyric: 'Georgia Gwinnett, with love and respect'},
+        {delta: 67, lyric: 'Our alma mater, Georgia Gwinnett'},
+        {delta: 75, lyric: 'Our alma mater, Georgia Gwinnett'}
+      ];
+      break;
+    case 'vocal':
+      lyrics = [
+        {delta: 0, lyric: '[Introduction]'},
+        {delta: 12, lyric: 'We have gained wisdom and honor'},
+        {delta: 16, lyric: 'From our home of green and grey'},
+        {delta: 20, lyric: 'We will go forth and remember'},
+        {delta: 25, lyric: 'All we\'ve learned along the way'},
+        {delta: 29, lyric: 'And with knowledge and compassion'},
+        {delta: 34, lyric: 'We will build communities'},
+        {delta: 38, lyric: 'Leading by example'},
+        {delta: 42, lyric: 'And with dignity'},
+        {delta: 46, lyric: 'Georgia Gwinnett, we\'ll never forget'},
+        {delta: 54, lyric: 'How we have grown, and those that we\'ve met'},
+        {delta: 63, lyric: 'Georgia Gwinnett, with love and respect'},
+        {delta: 70, lyric: 'Our alma mater, Georgia Gwinnett'},
+        {delta: 78, lyric: 'Our alma mater, Georgia Gwinnett'}
+      ];
+      break;
+    default:
+      lyrics = [
+        {delta: 0, lyric: '[Introduction]'},
+        {delta: 8, lyric: 'We have gained wisdom and honor'},
+        {delta: 13, lyric: 'From our home of green and grey'},
+        {delta: 17, lyric: 'We will go forth and remember'},
+        {delta: 22, lyric: 'All we\'ve learned along the way'},
+        {delta: 25, lyric: 'And with knowledge and compassion'},
+        {delta: 29, lyric: 'We will build communities'},
+        {delta: 34, lyric: 'Leading by example'},
+        {delta: 39, lyric: 'And with dignity'},
+        {delta: 42, lyric: 'Georgia Gwinnett, we\'ll never forget'},
+        {delta: 51, lyric: 'How we have grown, and those that we\'ve met'},
+        {delta: 59, lyric: 'Georgia Gwinnett, with love and respect'},
+        {delta: 67, lyric: 'Our alma mater, Georgia Gwinnett'},
+        {delta: 75, lyric: 'Our alma mater, Georgia Gwinnett'}
+      ];
+  }
+
+  return lyrics;
 }
 
 function updateSelected(id) {
@@ -15,20 +115,65 @@ function updateSelected(id) {
   newButton.classList.toggle(selectedButton, true);
 }
 
+function updatePos(delta, state) {
+  var pos = document.getElementById('pos');
+  pos.innerHTML = '<span>Position:</span> ' + delta + '/' + length + ' (' + state + ')';
+
+  var tick = document.getElementsByClassName('tick')[0];
+  tick.width = (delta / length) * 100 + '%';
+
+  var lyrics = getLyrics();
+  var curLyric = 0;
+  for (var i = 0; i < lyrics.length; i++) {
+    if (lyrics[i].delta <= delta) {
+      curLyric = i;
+    }
+  }
+
+  var previous = document.getElementById('previous');
+  var current = document.getElementById('current');
+  var next = document.getElementById('next');
+
+  current.innerHTML = lyrics[curLyric].lyric;
+  if (curLyric > 0) {
+    previous.innerHTML = lyrics[curLyric-1].lyric;
+    next.innerHTML = lyrics[curLyric + 1].lyric;
+  }
+  else {
+    previous.innerHTML = '--';
+    next.innerHTML = '--';
+  }
+}
+
 function instrumental() {
   updateSelected('instrumental');
+  var music = 'lib/music/Alma Mater PIano.mp3';
+  length = 93;
+  updatePos(0, '-');
 }
 
 function lowVocal() {
   updateSelected('low');
+  var music = 'lib/music/Alma Mater PIano.mp3';
+  length = 89;
+
+  updatePos(0, '-');
 }
 
 function highVocal() {
   updateSelected('high');
+  var music = 'lib/music/Alma Mater PIano.mp3';
+  length = 89;
+
+  updatePos(0, '-');
 }
 
 function vocal() {
   updateSelected('vocal');
+  var music = 'lib/music/Alma Mater PIano.mp3';
+  length = 91;
+
+  updatePos(0, '-');
 }
 
 function trackPlay() {
