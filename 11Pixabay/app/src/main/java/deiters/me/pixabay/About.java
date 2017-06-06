c59fbbcd4a9f1a21ad18cfbbed57fe9b7686689d
MDeiters <mdeiters@ITECLabAdmins-MacBook-Pro.local>
2017-06-06 16:49:42 -0400

    Pixabay

--- 11Pixabay/app/src/main/java/deiters/me/pixabay/About.java
+++ 11Pixabay/app/src/main/java/deiters/me/pixabay/About.java
@@ -1,7 +1,11 @@
 package deiters.me.pixabay;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class About extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class About extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        ImageView pixabay = (ImageView) findViewById(R.id.pixabayLogo);
+
+        pixabay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToUrl("https://pixabay.com");
+            }
+        });
+    }
+
+    private void goToUrl (String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
     }
 }
--- 11Pixabay/app/src/main/java/deiters/me/pixabay/MainActivity.java
+++ 11Pixabay/app/src/main/java/deiters/me/pixabay/MainActivity.java
@@ -1,24 +1,37 @@
 package deiters.me.pixabay;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
-import org.json.JSONObject;
-
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "Pixabay";
+    private static PixabayQueryResult result;
+    private ImageView imageView;
+    private ConstraintLayout constrainLayout;
+    private TextToSpeech tts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,20 +40,36 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        imageView = (ImageView) findViewById(R.id.imageView);
+        constrainLayout = (ConstraintLayout) findViewById(R.id.contraintLayout);
+
        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String service = "https://pixabay.com/api/";
-                String key = "key=5555459-b4fbd09dbba0b6b6dd5a296ee";
-                String query_params = "&editors_choice=true&safesearch=true&image_type=photos";
-                String urlString = service + "?" + key + query_params;
                 PixabayFetchTask task = new PixabayFetchTask();
-                task.doInBackground(urlString);
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
+                String service = "https://pixabay.com/api/";
+                String key = "5555459-b4fbd09dbba0b6b6dd5a296ee";
+                String query_params = "&editor_choice=true&safesearch=true&image_type=photo";
+                String urlString = service + "?key=" + key + query_params;
+                task.execute(urlString);
             }
         });
+
+        tts=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    tts.setLanguage(Locale.US);
+                }
+            }
+        });
+    }
+
+    public void showTags(String str) {
+        Snackbar snackbar = Snackbar.make(constrainLayout, str, Snackbar.LENGTH_LONG);
+        snackbar.show();
+        tts.speak(str, TextToSpeech.QUEUE_FLUSH, null);
     }
 
     @Override
@@ -59,36 +88,54 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_about) {
+            startActivity(new Intent(MainActivity.this, About.class));
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    class PixabayFetchTask extends AsyncTask<String, Void, Bitmap> {
+    public static long getRandomLong(long min, long max) {
+        return (long) (Math.random() * (max - min)) + min;
+    }
+
+    class PixabayFetchTask extends AsyncTask<String, Void, PixabayQueryResult> {
 
         @Override
-        protected Bitmap doInBackground(String... params) {
-            Log.v("Pixabay", "Running doInBackground()");
-            Log.v("Pixabay", "String[0] = " + params[0]);
+        protected PixabayQueryResult doInBackground(String... params) {
 
-            try {
-                URL url = new URL(params[0]);
+            if (result == null || result.isExpired()) {
 
+                Log.v("Pixabay", "String[0] =" + params[0]);
 
-                JSONObject json = new JSONObject();
-                Log.v("Pixabay", "json = " + json);
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
+                try {
+                    String line;
+                    URL u = new URL(params[0]);
+                    HttpURLConnection conn = (HttpURLConnection) u.openConnection();
+                    InputStream in = new BufferedInputStream(conn.getInputStream());
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                    StringBuilder json = new StringBuilder();
+                    while ((line = reader.readLine()) != null) json.append(line);
+                    Log.v("Pixabay", "json =" + json);
+
+                    result = new PixabayQueryResult(json.toString());
+                    result.setResponseCode(conn.getResponseCode());
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
 
-            return null;
+            return result;
         }
 
         @Override
-        protected void onPostExecute(Bitmap bitmap) {
-            super.onPostExecute(bitmap);
-            Log.v("Pixabay", "Running onPostExecute()");
+        protected void onPostExecute(PixabayQueryResult result) {
+            super.onPostExecute(result);
+            int selected = (int)getRandomLong(0, result.size());
+            Bitmap bitmap = result.getBitmap(selected);
+            imageView.setImageBitmap(bitmap);
+            showTags(result.getTags(selected));
         }
     }
 }
